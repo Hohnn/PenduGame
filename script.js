28e184d1e7e844e7f7ab6b6fcf0dbda59dd36c08
Hohnn <jean-bateee@hotmail.fr>
2021-05-09 22:13:12 +0200

    before responsive

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const myWords = [
     "HOLA",
 ]
 
+const game = ["FORTNITE", "BIOSHOCK", "DISHONORED", "BLOODBORNE", "MINECRAFT", "HADES", "TETRIS", "NIER"]
+
+const cLaMerde = [myWords, game]
+
 var word = document.getElementById("word");
 let wordselected;
 let wordLength;
@@ -21,6 +25,7 @@ retry.addEventListener('click', () => {
     myblur.classList.remove('blur')
     finish.classList.add('d-none')
     output.classList.remove('crash')
+    output.classList.remove('go')
 })
 // PAS FINI !! au click lancer une recherche aléatoire des mots, puis afficher le mot selectionné au tableau
 const start = document.getElementById("start")
@@ -28,6 +33,7 @@ start.addEventListener("click", () => {
     recommence()
     start.innerHTML = 'RESTART'
     output.classList.add('starting')
+    playBgSound()
 });
 
 function recommence() {
@@ -38,16 +44,19 @@ function recommence() {
     while (el.firstChild) {
         el.removeChild(el.firstChild);
     }
-    let randomisation = Math.floor(Math.random() * myWords.length);
+    let randomisationListe = Math.floor(Math.random() * cLaMerde.length);
+    let randomisation = Math.floor(Math.random() * cLaMerde[randomisationListe].length);
+    
     console.log(randomisation)
-    if (wordselected == myWords[randomisation]) {
+    console.log(randomisationListe)
+    if (wordselected == cLaMerde[randomisationListe][randomisation]) {
         if (randomisation == 0) {
             randomisation++
         } else {
             randomisation--
         }
     }
-    wordselected = myWords[randomisation];
+    wordselected = cLaMerde[randomisationListe][randomisation];
     wordLength = wordselected.length;
     console.log(wordselected);
     console.log(wordLength);
@@ -73,68 +82,58 @@ let myWin = 0;
 let myloose = 0;
 let perdu;
 let gg = 0
-let difficulty = 8
+let difficulty = 7
 const ennemi = document.getElementById("ennemi")
 const miss2 = document.getElementById('miss2')
 // recupérer la lettre en cliquant sur le bouton
 letterOne.forEach(element => (
     element.addEventListener("click", function () {
-            let letter = this.dataset.translate;
-            console.log(word)
-            shoot.classList.remove('anim')
-            void shoot.offsetWidth;
-            shoot.classList.add('anim')
-            gg = 0
-            for (let i = 0; i < wordLength; i++) {
-                console.log(element)
-                
-                console.log(word.childNodes[i].innerHTML)
-                if (element.innerHTML == word.childNodes[i].innerHTML) {
-                    console.log("ca marche")
+        shot()
+        shoot.classList.remove('anim')
+        void shoot.offsetWidth;
+        shoot.classList.add('anim')
+        gg = 0
 
-                    setTimeout(function () {
-
-                        word.childNodes[i].classList.remove("transparent");
-                        element.classList.add("transparent2");
-                        ennemi.classList.remove('hit')
-                        void ennemi.offsetWidth;
-                        ennemi.classList.add('hit')
-                    }, 900);
-                    
-                    myWin++
-                } else {
-                    element.classList.add("false");
-                    gg++
-
+        for (let i = 0; i < wordLength; i++) {
+            if (element.innerHTML == word.childNodes[i].innerHTML) {
+                setTimeout(function () {
+                    word.childNodes[i].classList.remove("transparent");
+                    element.classList.add("transparent2");
+                    ennemi.classList.remove('hit')
+                    void ennemi.offsetWidth;
+                    ennemi.classList.add('hit')
+                    impact()
+                    if (myWin == wordLength) {
+                        myWin = 0;
+                        myloose = 0;
+                        finishState.innerHTML = 'You Win ! <br>' + wordselected + ' IS DESTROY !'
+                        finish.classList.remove('d-none')
+                        myblur.classList.add('blur')
+                        output.classList.add('crash')
+                        finishAudio()
+                    }
+                }, 900);
+                myWin++
+            } else {
+                element.classList.add("false");
+                gg++
+                if (myloose == difficulty) {
+                    myWin = 0;
+                    myloose = 0;
+                    finishState.innerHTML = 'You failed ! <br> The enemy was ' + wordselected
+                    finish.classList.remove('d-none')
+                    myblur.classList.add('blur')
+                    output.classList.add('go')
+                    finishAudio()
                 }
             }
-            if (gg == wordLength) {
-                myloose++
-                miss2.innerHTML = 'Misses : ' + myloose
-
-            }
-            if (myWin == wordLength) {
-                myWin = 0;
-                myloose = 0;
-                finishState.innerHTML = 'You Win ! <br>' + wordselected + ' is destroy'
-                finish.classList.remove('d-none')
-                myblur.classList.add('blur')
-                output.classList.add('crash')
-                
-            }
-            console.log(myloose);
-            if (myloose == difficulty) {
-                myWin = 0;
-                myloose = 0;
-                finishState.innerHTML = 'You fail ! <br> The enemy was ' + wordselected
-                finish.classList.remove('d-none')
-                myblur.classList.add('blur')
-
-            }
-
         }
 
-    )
+        if (gg == wordLength) {
+            myloose++
+            miss2.innerHTML = 'Misses : ' + myloose
+        }
+    })
 ));
 
 const miss = document.getElementById('miss')
@@ -165,3 +164,35 @@ normal.addEventListener("click", () => {
     hard.classList.remove("difficulty");
     miss.innerHTML = 'Misses allowed : ' + difficulty
 })
+const bgSound = document.getElementById('bgSound')
+function playBgSound() {   
+    bgSound.play()
+    bgSound.volume = 0.2
+}
+
+const mute = document.getElementById('soundMute')
+mute.addEventListener('click', () => {
+    stopBgSound()
+})
+function stopBgSound() {
+    bgSound.pause()
+    bgSound.currentTime = 0;
+}
+
+const impactSound = document.getElementById("impactSound")
+function impact() {
+    shotSound.play()
+    shotSound.volume = 0.1
+}
+
+const shotSound = document.getElementById("shotSound")
+function shot() {
+    impactSound.play()
+    impactSound.volume = 0.1
+}
+const finishSound = document.getElementById("finishSound")
+function finishAudio() {
+    finishSound.play()
+    finishSound.volume = 0.15
+}
+
